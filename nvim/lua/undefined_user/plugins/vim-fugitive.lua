fb68b226bb8f60c26a7011bd900c5dfdd3462c5e
Mer Thich <mthich15@gmail.com>
2024-04-03 22:27:33 -0700

    Fugitive is not that girs. Still here but not that girl

--- nvim/lua/undefined_user/plugins/vim-fugitive.lua
+++ nvim/lua/undefined_user/plugins/vim-fugitive.lua
@@ -3,12 +3,5 @@ return {
 
     config = function()
         vim.keymap.set("n", "<leader>fg", vim.cmd.Git)
-
-        vim.keymap.set("n", "<leader>p", vim.cmd.Git('push'))
-
-        vim.keymap.set("n", "<leader>t", ":Git push -u origin ");
-
-        vim.keymap.set("n", "gu", "<cmd>diffget //2<CR>")
-        vim.keymap.set("n", "gh", "<cmd>diffget //3<CR>")
     end
 }
